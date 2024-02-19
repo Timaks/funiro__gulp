0a3fbfd9d2e0cc8931455e34b7c25c6ddb5546b3
Timaks <aksinya2016@gmail.com>
2024-02-19 17:45:23 +0300

    slider_part1

--- #src/js/app.js
+++ #src/js/app.js
@@ -1,5 +1,6 @@
 // подкл файлы в которых работаем. из папки files/
 @@include('files/regular.js', {})
+@@include('files/sliders.js', {})
 @@include('files/functions.js', {})
 @@include('files/script.js', {})
 @@include('files/forms.js', {})
