6ca2392bc48701a3afb5180f42256e4a81dbf9e0
Timaks <aksinya2016@gmail.com>
2024-02-21 16:25:54 +0300

    slider_part2 finish

--- #src/js/app.js
+++ #src/js/app.js
@@ -1,5 +1,6 @@
 // подкл файлы в которых работаем. из папки files/
 @@include('files/regular.js', {})
+@@include('files/dynamic_adapt.js', {})
 @@include('files/sliders.js', {})
 @@include('files/functions.js', {})
 @@include('files/script.js', {})
