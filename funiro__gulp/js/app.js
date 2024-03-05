977d2846bc7482a2321c694b12de0082070d656e
Timaks <aksinya2016@gmail.com>
2024-03-05 16:44:14 +0300

    загрузка  продуктов из файла json

--- funiro__gulp/js/app.js
+++ funiro__gulp/js/app.js
@@ -1072,6 +1072,11 @@ window.onload = function(){
         } else if(!targetElement.closest('.search-form') && document.querySelector('.search-form._active')){
             document.querySelector('.search-form').classList.remove('_active');
         }
+        if (targetElement.classList.contains('products__more'))
+        {
+            getProducts(targetElement);
+            e.preventDefault();
+        }
     }   
     //header
 
@@ -1088,7 +1093,125 @@ window.onload = function(){
 
     const headerObserver = new IntersectionObserver(callback);
     headerObserver.observe(headerElement);
+
+
+    //products__more 
+    //в саму ф-цию получаем кнопку show more
+
+    async function getProducts(button){
+        //если нет класса hold, мы его добавляем
+        if (!button.classList.contains('_hold')){
+            button.classList.add('_hold');
+            //путь к нашему файлу (в реальном проекте адрес сервера и передаваемые параметры)
+            const file = "json/products.json";
+        //выполняем GET запрос с помощью fetch
+            let response = await fetch(file, {
+                method: "GET"
+            });
+            //проверка
+            if(response.ok){
+                //получаем содержание файла в формате json
+                let result = await response.json();
+                loadProducts(result);
+                //после этого убираем класс _hold и удаляем кнопку
+                button.classList.remove('_hold');
+            //чтобы не подгружал одни и те же продукты
+                button.remove();
+            } else {
+                alert("Ошибка");
+            }
+        }
+    }
+    function loadProducts(data){
+        const productsItems = document.querySelector('.products__items');
+        data.products.forEach(item => {
+            const productId = item.id;
+            const productUrl = item.url;
+            const productImage = item.image;
+            const productTitle = item.title;
+            const productText = item.text;
+            const productPrice = item.price;
+            const productOldPrice = item.priceOld;
+            const productShareUrl = item.shareUrl;
+            const productLikeUrl = item.likeUrl;
+            const productLabels = item.labels;
+
+            let productTemplateStart = `<article data-pid="${productId}" class="products__item item-product">`;
+			let productTemplateEnd = `</article>`;
+
+			let productTemplateLabels = '';
+			if (productLabels) {
+				let productTemplateLabelsStart = `<div class="item-product__labels">`;
+				let productTemplateLabelsEnd = `</div>`;
+				let productTemplateLabelsContent = '';
+
+				productLabels.forEach(labelItem => {
+					productTemplateLabelsContent += `<div class="item-product__label item-product__label_${labelItem.type}">${labelItem.value}</div>`;
+				});
+
+				productTemplateLabels += productTemplateLabelsStart;
+				productTemplateLabels += productTemplateLabelsContent;
+				productTemplateLabels += productTemplateLabelsEnd;
+			}
+
+			let productTemplateImage = `
+		<a href="${productUrl}" class="item-product__image _ibg">
+			<img src="img/products/${productImage}" alt="${productTitle}">
+		</a>
+	`;
+
+			let productTemplateBodyStart = `<div class="item-product__body">`;
+			let productTemplateBodyEnd = `</div>`;
+
+			let productTemplateContent = `
+		<div class="item-product__content">
+			<h5 class="item-product__title">${productTitle}</h5>
+			<div class="item-product__text">${productText}</div>
+		</div>
+	`;
+
+			let productTemplatePrices = '';
+			let productTemplatePricesStart = `<div class="item-product__prices">`;
+			let productTemplatePricesCurrent = `<div class="item-product__price">Rp ${productPrice}</div>`;
+			let productTemplatePricesOld = `<div class="item-product__price item-product__price_old">Rp ${productOldPrice}</div>`;
+			let productTemplatePricesEnd = `</div>`;
+
+			productTemplatePrices = productTemplatePricesStart;
+			productTemplatePrices += productTemplatePricesCurrent;
+			if (productOldPrice) {
+				productTemplatePrices += productTemplatePricesOld;
+			}
+			productTemplatePrices += productTemplatePricesEnd;
+
+			let productTemplateActions = `
+		<div class="item-product__actions actions-product">
+			<div class="actions-product__body">
+				<a href="" class="actions-product__button btn btn_white">Add to cart</a>
+				<a href="${productShareUrl}" class="actions-product__link _icon-share">Share</a>
+				<a href="${productLikeUrl}" class="actions-product__link _icon-favorite">Like</a>
+			</div>
+		</div>
+	`;
+
+			let productTemplateBody = '';
+			productTemplateBody += productTemplateBodyStart;
+			productTemplateBody += productTemplateContent;
+			productTemplateBody += productTemplatePrices;
+			productTemplateBody += productTemplateActions;
+			productTemplateBody += productTemplateBodyEnd;
+
+			let productTemplate = '';
+			productTemplate += productTemplateStart;
+			productTemplate += productTemplateLabels;
+			productTemplate += productTemplateImage;
+			productTemplate += productTemplateBody;
+			productTemplate += productTemplateEnd;
+
+            productsItems.insertAdjacentHTML('beforeend', productTemplate);
+            });
+    }
 }
+
 //let btn = document.querySelectorAll('button[type="submit"],input[type="submit"]');
 let forms = document.querySelectorAll('form');
 if (forms.length > 0) {
