c4a5c34cd009bf783afe1c2a2b80c79cbb4f8061
Timaks <aksinya2016@gmail.com>
2024-03-13 17:59:23 +0300

    Верстка галереи Furniture (конец проекта)

--- funiro__gulp/js/app.js
+++ funiro__gulp/js/app.js
@@ -1410,6 +1410,64 @@ window.onload = function(){
             }
         }
     }
+
+
+
+
+    //Furniture gallery
+    const furniture = document.querySelector('.furniture__body');
+    if(furniture && !isMobile.any()) {
+         // проверка на существование абъекта и то что это не тач скрин
+        const furnitureItems = document.querySelector('.furniture__items');
+        const furnitureColumn = document.querySelectorAll('.furniture__column');
+        
+        //Скорость анимации
+        const speed = furniture.dataset.speed;
+
+         //Объявление переменных
+        let positionX = 0;
+        
+        let coordXprocent = 0;
+
+        function setMouseGalleryStyle() {
+            let furnitureItemsWidth = 0;
+            //вычисляем актуальный размер всего контента
+            furnitureColumn.forEach(element => {
+                furnitureItemsWidth += element.offsetWidth;
+            });
+
+            //получаем разницу ширин всего контента и видимой части (движение за курсором)
+            const furnitureDifferent = furnitureItemsWidth - furniture.offsetWidth;
+            const distX = Math.floor(coordXprocent - positionX);
+ //  с учетом скорости добавляем в переменную positionX потом относительно разницы ширин
+            positionX = positionX + (distX * speed);
+            let position = furnitureDifferent / 200 * positionX;
+
+            furnitureItems.style.cssText = `transform: translate3d(${-position}px,0,0);`;
+
+            if (Math.abs(distX) > 0) {
+                requestAnimationFrame(setMouseGalleryStyle);
+            } else {
+                furniture.classList.remove('_init');
+            }
+        }
+
+        furniture.addEventListener("mousemove", function (e) {
+             //получение ширины
+            const furnitureWidth = furniture.offsetWidth;
+
+             //ноль по середине
+            const coordX = e.pageX - furnitureWidth / 2;
+
+             //получаем проценты
+            coordXprocent = coordX / furnitureWidth * 200;
+
+            if (!furniture.classList.contains('_init')) {
+                requestAnimationFrame(setMouseGalleryStyle);
+                furniture.classList.add('_init');
+            }
+        });
+    }
 }
 //let btn = document.querySelectorAll('button[type="submit"],input[type="submit"]');
 let forms = document.querySelectorAll('form');
